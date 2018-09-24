78f56df4f330a3b7badb406342dda2f840c966cf
pengyi <pengyi@soundbus.cn>
2018-09-24 20:26:32 +0800

    电影播放及预告模型

--- app/models.py
+++ app/models.py
@@ -38,3 +38,45 @@ class Userlog(db.Model):
 
     def __repr__(self):
         return "<User %r>" % self.id
+
+
+class Tag(db.Model):
+    __tablename__ = "tag"
+    id = db.Column(db.Interger, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    movies = db.relationship("Movie", backref="tag") # 电影外键关联
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id = db.Column(db.Interger, primary_key=True)   # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    url = db.Column(db.String(255), unique=True)    # 地址
+    info = db.Column(db.Text)   # 简介
+    logo = db.Column(db.String(255), unique=True)   # 封面
+    star = db.Column(db.SmallInterger)  # 星级
+    playnum = db.Column(db.BigInterger) # 播放量
+    commentnum = db.Column(db.BigInterger)  # 评论量
+    tag_id = db.Column(db.Interger, db.ForeignKey('tag.id'))    # 所属标签
+    area = db.Column(db.String(255))    # 地区
+    release_time = db.Column(db.Date)   # 上映时间
+    length = db.Column(db.String)   # 播放时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)   # 添加时间
+
+    def __repr__(self):
+        return "<Moive %r>" % self.title
+
+
+class Preview(db.Model):
+    __tablename__ = "preview"
+    id = db.Column(db.Interger, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Preview %r>" % self.title
