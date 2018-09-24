5e23b15ce053b683a18738c6f981a3eb530a6079
pengyi <pengyi@soundbus.cn>
2018-09-24 21:34:05 +0800

    修改小bug创建数据库

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config["SQLACHEMY_DATABASE_URI"] = "pymysql://root:mysql@127.0.0.1:3306/movie"
-app.config["SQLACHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:mysql@localhost:3306/movie'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
@@ -66,7 +66,7 @@ class Movie(db.Model):
     tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))    # 所属标签
     area = db.Column(db.String(255))    # 地区
     release_time = db.Column(db.Date)   # 上映时间
-    length = db.Column(db.String)   # 播放时间
+    length = db.Column(db.String(100))   # 播放时间
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)   # 添加时间
     comments = db.relationship("Comment", backref='movie')   # 评论外键关联关系
     moviecol = db.relationship("Moviecol", backref='movie')  # 收藏外键关联关系
