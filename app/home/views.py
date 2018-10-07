5db592e8fe13f708afa3daaebaf76bf3b65effa9
pengyi <535719197@qq.com>
2018-10-07 21:50:24 +0800

    增加电影列表及搜索页面

--- app/home/views.py
+++ app/home/views.py
@@ -6,35 +6,47 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template("home/index.html")
 
+
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+
 # 登陆
 @home.route("/login/")
 def login():
     return render_template("home/login.html")
+
 
 # 登出
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home/logout.html"))
 
+
 # 注册
 @home.route("/register/")
 def register():
     return render_template("home/register.html")
+
 
 # 会员中心
 @home.route("/user/")
 def user():
     return render_template("home/user.html")
 
+
 # 修改密码
 @home.route("/pwd/")
 def pwd():
     return render_template("home/pwd.html")
 
+
 # 评论
 @home.route("/comments/")
 def comments():
     return render_template("home/comments.html")
+
 
 # 登陆日志
 @home.route("/loginlog/")
@@ -45,3 +57,9 @@ def loginlog():
 @home.route("/moviecol/")
 def moviecol():
     return render_template("home/moviecol.html")
+
+
+# 电影搜索
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
