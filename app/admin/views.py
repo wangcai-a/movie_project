b50cf92b688de2fa92c8bf8958f7e8b3e879cda5
pengyi <535719197@qq.com>
2018-10-14 22:36:11 +0800

    电影管理页面

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,32 +2,51 @@ from . import admin
 from flask import render_template, url_for, redirect
 
 
+# 主页面
 @admin.route("/")
 def index():
     return render_template("admin/index.html")
 
 
+# 登入
 @admin.route("/login")
 def login():
     return render_template("admin/login.html")
 
 
+# 登出
 @admin.route("/logout")
 def logout():
     return redirect(url_for("admin.logout"))
 
 
+# 密码修改
 @admin.route("/pwd")
 def pwd():
     return render_template("admin/pwd.html")
 
 
+# 标签添加
 @admin.route("/tag/add")
 def tag_add():
     return render_template("admin/tag_add.html")
 
 
+# 标签列表
 @admin.route("/tag/list")
 def tag_list():
     return render_template("admin/tag_list.html")
+
+
+# 编辑电影
+@admin.route("/movie/add")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+
+# 编辑电影
+@admin.route("/movie/list")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
 
