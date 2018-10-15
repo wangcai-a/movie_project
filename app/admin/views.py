e61930859a10e0d1c9375250aff335ecbb756116
pengyi <535719197@qq.com>
2018-10-15 10:17:26 +0800

    收藏管理页面

--- app/admin/views.py
+++ app/admin/views.py
@@ -73,3 +73,9 @@ def admin_list():
 def comment_list():
     return render_template("admin/comment_list.html")
 
+
+# 会员列表
+@admin.route("/moviecol/list")
+def moviecol_list():
+    return render_template("admin/moviecol_list.html")
+
